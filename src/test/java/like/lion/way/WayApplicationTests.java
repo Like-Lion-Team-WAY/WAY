95a86721d2572665d8d3ed7f6fde7426ee910618
Firemanhyeon <ghgus990129@naver.com>
2024-07-30 09:17:40 +0900

    first commit

---
+++ src/test/java/like/lion/way/WayApplicationTests.java
@@ -0,0 +1,13 @@
+package like.lion.way;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
