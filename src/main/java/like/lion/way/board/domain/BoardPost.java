a0e724a190d07e7c091ea82070c7aee85f441759
LeeYuJoon <leeyujoon97@gmail.com>
2024-07-30 15:43:38 +0900

    feat: Board Entity 생성 (#10)

---
+++ src/main/java/like/lion/way/board/domain/BoardPost.java
@@ -0,0 +1,51 @@
+package like.lion.way.board.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BoardPost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_post_id")
+    private Long id;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @Column(name = "board_post_title", nullable = false)
+    private String title;
+
+    @CreationTimestamp
+    @Column(name = "board_post_created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Builder
+    public BoardPost(Board board, String title) {
+
+        this.board = board;
+        this.title = title;
+
+    }
+
+}
---
+++ src/main/java/like/lion/way/board/domain/BoardPostLike.java
@@ -0,0 +1,41 @@
+package like.lion.way.board.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BoardPostLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_post_like_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_post_id")
+    private BoardPost boardPost;
+
+    @Column(name = "board_post_like_user_id", nullable = false)
+    private Long userId;
+
+    @Builder
+    public BoardPostLike (BoardPost post, Long userId) {
+
+        this.boardPost = post;
+        this.userId = userId;
+
+    }
+
+}
