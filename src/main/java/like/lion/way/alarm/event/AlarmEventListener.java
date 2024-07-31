68556eb0902400dcb3c07e19f68cf6e3d31ee738
hye2021 <hyebeenbyeon@gmail.com>
2024-07-31 11:19:16 +0900

    feat: create config file for async thread (#14)

--- src/main/java/like/lion/way/alarm/event/AlarmEventListener.java
+++ src/main/java/like/lion/way/alarm/event/AlarmEventListener.java
@@ -2,11 +2,13 @@ package like.lion.way.alarm.event;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class AlarmEventListener {
+    @Async
     @EventListener
     public void handleAlarmEvent(AlarmEvent alarmEvent) {
         // SSE를 사용하여 클라이언트로 알람 전송하기
