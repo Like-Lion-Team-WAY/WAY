fc0cfbc4f8f3e70499de77260d63d4ad00f03fd7
LEEYUJOON <123285545+L-U-Ready@users.noreply.github.com>
2024-07-31 15:11:53 +0900

    [Feature] implement crud api about community board (#26)
    * feat: 게시판에 필요한 crud api 구현 (#22)
    
    * refactor: 게시판에 필요한 의존성 추가. 기타 코드 리팩토링 (#22)
    
    * refactor: BoardEditServiceRequest 오류 수정 (#22)

---
+++ src/main/java/like/lion/way/board/api/request/BoardCreateRequest.java
@@ -0,0 +1,52 @@
+package like.lion.way.board.api.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import like.lion.way.board.application.request.BoardCreateServiceRequest;
+import like.lion.way.board.domain.AnonymousPermission;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class BoardCreateRequest {
+
+    @NotBlank(message = "게시판 이름은 필수입니다.")
+    private String name;
+
+    @NotBlank(message = "게시판 설명은 필수입니다.")
+    private String introduction;
+
+    @NotBlank(message = "익명 허용 여부는 필수입니다.")
+    private AnonymousPermission anonymousPermission;
+
+    @NotNull(message = "관리 유저는 필수입니다.")
+    private Long userId;
+
+    @Builder
+    public BoardCreateRequest(
+            String name,
+            String introduction,
+            AnonymousPermission anonymousPermission,
+            Long userId) {
+
+        this.name = name;
+        this.introduction = introduction;
+        this.anonymousPermission = anonymousPermission;
+        this.userId = userId;
+
+    }
+
+    public BoardCreateServiceRequest toServiceRequest() {
+
+        return BoardCreateServiceRequest.builder()
+                .name(name)
+                .introduction(introduction)
+                .anonymousPermission(anonymousPermission)
+                .userId(userId)
+                .build();
+
+    }
+
+}
