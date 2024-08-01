a660df4957d5795fe6f40ca949e3a350512aad69
leenayeonnn <skdus4190@gmail.com>
2024-08-01 14:29:57 +0900

    feat: producer 에서 message db 저장 기능 추가 (#30)

--- src/main/java/like/lion/way/chat/service/kafka/imple/ProducerImpl.java
+++ src/main/java/like/lion/way/chat/service/kafka/imple/ProducerImpl.java
@@ -3,7 +3,9 @@ package like.lion.way.chat.service.kafka.imple;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDateTime;
 import like.lion.way.chat.domain.Message;
+import like.lion.way.chat.repository.MessageRepository;
 import like.lion.way.chat.service.kafka.Producer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -15,10 +17,14 @@ public class ProducerImpl implements Producer {
     private static final String TOPIC = "topic-messages";
     private final KafkaTemplate<String, String> kafkaTemplate;
     private final ObjectMapper objectMapper; // JSON 변환을 위한 ObjectMapper
+    private final MessageRepository messageRepository;
 
     @Override
     public void sendMessage(Message message) {
         try {
+            message.setCreatedAt(LocalDateTime.now());
+            messageRepository.save(message);
+
             // Message 객체를 JSON 문자열로 변환
             System.out.println("[producer] sending message: " + message.getText());
             String messageAsString = objectMapper.writeValueAsString(message);
