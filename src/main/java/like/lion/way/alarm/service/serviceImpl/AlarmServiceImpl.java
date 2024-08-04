1ad2d1fe258f04f3d69ea5fd9a5b0df44fe433d5
hye2021 <hyebeenbyeon@gmail.com>
2024-08-04 22:22:07 +0900

    feat: improve AlarmType(Enum) with multiple fields (#34)

--- src/main/java/like/lion/way/alarm/service/serviceImpl/AlarmServiceImpl.java
+++ src/main/java/like/lion/way/alarm/service/serviceImpl/AlarmServiceImpl.java
@@ -28,8 +28,9 @@ public class AlarmServiceImpl implements AlarmService {
 
     @Override
     public Alarm createAlarm(AlarmEvent alarmEvent) {
-        String message = getAlarmMessage(alarmEvent.getType(), alarmEvent.getFromUser().getNickname());
-        String url = getAlarmUrl(alarmEvent.getType(), alarmEvent.getPathVariable());
+        AlarmType type = alarmEvent.getType();
+        String message = type.getMessage(alarmEvent.getFromUser().getNickname());
+        String url = type.getUrl(alarmEvent.getPathVariable());
         return new Alarm(alarmEvent.getToUser(), message, url);
     }
 
@@ -40,52 +41,13 @@ public class AlarmServiceImpl implements AlarmService {
     }
 
     private boolean getAlarmStatus(AlarmSetting alarmSetting, AlarmType alarmType) {
-        switch (alarmType) {
-            case NEW_QUESTION:
-                return alarmSetting.isNewQuestion();
-            case REPLY:
-                return alarmSetting.isReply();
-            case COMMENT:
-                return alarmSetting.isComment();
-            case ANSWER:
-                return alarmSetting.isAnswer();
-            case BOARD_COMMENT:
-                return alarmSetting.isBoardComment();
-            default:
-                return false;
-        }
+        return switch (alarmType) {
+            case NEW_QUESTION -> alarmSetting.isNewQuestion();
+            case REPLY -> alarmSetting.isReply();
+            case COMMENT -> alarmSetting.isComment();
+            case ANSWER -> alarmSetting.isAnswer();
+            case BOARD_COMMENT -> alarmSetting.isBoardComment();
+            default -> false;
+        };
     }
-
-    private String getAlarmMessage(AlarmType type, String fromUser) {
-        switch (type) {
-            case NEW_QUESTION:
-                return "새 질문이 도착했습니다.";
-            case REPLY:
-                return fromUser + " 님이 회원님이 남긴 댓글에 대댓글을 달았습니다.";
-            case COMMENT:
-                return fromUser + " 님이 회원님의 글에 댓글을 달았습니다.";
-            case ANSWER:
-                return fromUser + " 님이 회원님이 남긴 질문에 답변을 달았습니다.";
-            case BOARD_COMMENT:
-                return fromUser + " 님이 회원님의 커뮤니티 게시글에 댓글을 달았습니다.";
-            default:
-                return "";
-        }
-    }
-
-    private String getAlarmUrl(AlarmType type, String pathVariable) {
-        switch (type) {
-            case NEW_QUESTION:
-                return "/questions/NewList/" + pathVariable;
-            case REPLY, COMMENT:
-                return "/posts/detail/" + pathVariable;
-            case ANSWER:
-                return "";
-            case BOARD_COMMENT:
-                return "";
-            default:
-                return "";
-        }
-    }
-
 }
