74b854ba6e2ebda61ce10d52833203e12d89655b
LeeYuJoon <leeyujoon97@gmail.com>
2024-08-05 17:52:44 +0900

    feat: 게시판 상세보기 구현, postNum 수정 및 기타 리팩토링 (#49)

--- src/main/java/like/lion/way/board/api/BoardController.java
+++ src/main/java/like/lion/way/board/api/BoardController.java
@@ -4,15 +4,12 @@ import like.lion.way.board.application.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/boards")
 public class BoardController {
-
-    private final BoardService boardService;
 
     @GetMapping
     public String showBoardList() {
@@ -28,7 +25,7 @@ public class BoardController {
 
     }
 
-    @GetMapping("/setting")
+    @GetMapping("/setting/{boardName}")
     public String showBoardSettingForm() {
 
         return "pages/boards/boardSettingForm";
@@ -36,7 +33,7 @@ public class BoardController {
     }
 
     @GetMapping("/{boardName}")
-    public String  showPostForm(@PathVariable("boardName") String boardName) {
+    public String  showPostForm() {
 
         return "pages/boards/postForm";
 
@@ -46,6 +43,13 @@ public class BoardController {
     public String showPostCreateForm() {
 
         return "pages/boards/postCreateForm";
+
+    }
+
+    @GetMapping("/posts/{postTitle}")
+    public String showPostDetail() {
+
+        return "pages/boards/postDetailForm";
 
     }
 
