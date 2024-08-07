c1fc27c47d7c3bfc8afd03f5820801a72bd77674
hyejin <hyejin66@sookmyung.ac.kr>
2024-08-07 13:17:18 +0900

    feat: edit quesiton scrap (#51)

--- src/main/java/like/lion/way/feed/controller/QuestionBoxController.java
+++ src/main/java/like/lion/way/feed/controller/QuestionBoxController.java
@@ -4,13 +4,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class QuestionBoxController {
     @PostMapping("/questions/archieve")
-    public String archieveQuestion() {
+    public String archieveQuestion(@RequestParam("questionId") Long questionId, @RequestParam("userId") Long userId) {
+
         return null;
     }
 }
