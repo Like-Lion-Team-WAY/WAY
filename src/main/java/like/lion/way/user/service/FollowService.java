e0a49e4e6591b402e6bda0a785460d9c7c8c9c20
Firemanhyeon <ghgus990129@naver.com>
2024-08-09 22:01:09 +0900

    feat: follow logic modify

--- src/main/java/like/lion/way/user/service/FollowService.java
+++ src/main/java/like/lion/way/user/service/FollowService.java
@@ -2,13 +2,14 @@ package like.lion.way.user.service;
 
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
+import like.lion.way.user.domain.User;
 import like.lion.way.user.dto.FollowDto;
 import org.springframework.http.ResponseEntity;
 
 public interface FollowService {
-    List<FollowDto> getFollowerList(HttpServletRequest request);
+    List<FollowDto> getFollowerList(User user);
 
-    List<FollowDto> getFollowingList(HttpServletRequest request);
+    List<FollowDto> getFollowingList(User user);
 
     ResponseEntity<?> deleteFollower(HttpServletRequest request, String username);
 
