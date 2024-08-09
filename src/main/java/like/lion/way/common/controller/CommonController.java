130c2f5fb8b5eec1cd7536f7f86938333482ad8b
hyejin <hyejin66@sookmyung.ac.kr>
2024-08-09 20:25:45 +0900

    feat: implement main filter (로그인/비로그인에 따라 다르게 보이는 mainPage) (#70)

--- src/main/java/like/lion/way/common/controller/CommonController.java
+++ src/main/java/like/lion/way/common/controller/CommonController.java
@@ -1,11 +1,19 @@
 package like.lion.way.common.controller;
 
+import co.elastic.clients.elasticsearch.nodes.Http;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 import like.lion.way.feed.domain.Post;
+import like.lion.way.feed.domain.Question;
 import like.lion.way.feed.service.PostService;
 import like.lion.way.feed.service.QuestionService;
 import like.lion.way.jwt.util.JwtUtil;
+import like.lion.way.user.domain.Follow;
 import like.lion.way.user.domain.User;
+import like.lion.way.user.dto.FollowDto;
+import like.lion.way.user.service.FollowService;
+import like.lion.way.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -18,12 +26,48 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CommonController {
     private final PostService postService;
     private final QuestionService questionService;
+    private final UserService userService;
     private final JwtUtil jwtUtil;
+    private final FollowService followService;
+
+    private User getLoginUser(HttpServletRequest request) {
+        String token = jwtUtil.getCookieValue(request, "accessToken");
+        if (token == null || token.isEmpty()) {
+            System.out.println("Token is null or empty");
+            return null;
+        }
+        Long loginId = jwtUtil.getUserIdFromToken(token);
+        if (loginId == null) {
+            System.out.println("Login ID is null");
+            return null;
+        }
+        System.out.println(loginId);
+        return userService.findByUserId(loginId);
+    }
 
     @GetMapping("/main")
-    public String mainView(Model model){
-        model.addAttribute("posts", postService.getAllPosts());
-        model.addAttribute("questions", questionService.getAllQuestions());
+    public String mainView(Model model, HttpServletRequest request) {
+        User loginUser = getLoginUser(request);
+        if (loginUser == null) {
+            //비로그인 (우선 전체 게시글, 질문)
+            model.addAttribute("posts", postService.getAllPosts());
+            model.addAttribute("questions", questionService.getAllQuestions());
+        } else {
+            //로그인 (팔로우 한 사람들 게시글, 질문)
+            List<FollowDto> follows = followService.getFollowingList(request);
+            List<Post> posts= new ArrayList<>();
+            for (FollowDto follow : follows) {
+                User user = userService.findByUsername(follow.getUsername());
+                posts.addAll(postService.getPostByUser(user));
+            }
+            model.addAttribute("posts", posts);
+            List<Question> questions = new ArrayList<>();
+            for (FollowDto follow : follows) {
+                User user = userService.findByUsername(follow.getUsername());
+                questions.addAll(questionService.getQuestionByAnswerer(user));
+            }
+            model.addAttribute("questions", questions);
+        }
         return "pages/common/main";
     }
 }
