4d178959c56717fd7e278e716c9f2a45be3fb577
hye2021 <hyebeenbyeon@gmail.com>
2024-08-12 11:03:38 +0900

    feat: add a alarm event when an a comment or a precomment is  created (#58)

--- src/main/java/like/lion/way/feed/service/imp/PostCommentServiceImpl.java
+++ src/main/java/like/lion/way/feed/service/imp/PostCommentServiceImpl.java
@@ -1,13 +1,17 @@
 package like.lion.way.feed.service.imp;
 
 import java.time.LocalDateTime;
+import like.lion.way.alarm.domain.AlarmType;
+import like.lion.way.alarm.event.AlarmEvent;
 import like.lion.way.feed.domain.PostComment;
 import like.lion.way.feed.domain.dto.PostCommentDto;
 import like.lion.way.feed.repository.PostCommentRepository;
 import like.lion.way.feed.service.PostCommentService;
 import like.lion.way.feed.service.PostService;
+import like.lion.way.user.domain.User;
 import like.lion.way.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,22 +22,35 @@ public class PostCommentServiceImpl implements PostCommentService {
     private final PostCommentRepository postCommentRepository;
     private final PostService postService;
     private final UserService userService;
+    private final ApplicationEventPublisher publisher;
 
     @Override
     @Transactional
     public PostComment saveComment(Long postId, PostCommentDto postCommentDto, Long userId) {
-        PostComment postComment= new PostComment();
+        PostComment postComment = new PostComment();
         postComment.setPostCommentContent(postCommentDto.getPostCommentContent());
         postComment.setPostCommentCreatedAt(LocalDateTime.now());
         postComment.setPost(postService.getPostById(postId));
         postComment.setUser(userService.findByUserId(userId));
-        return postCommentRepository.save(postComment);
+        var value = postCommentRepository.save(postComment);
+
+        // 트랜잭션 종료 후 이벤트 발생
+        User fromUser = value.getUser();
+        User toUser = value.getPost().getUser();
+        String urlParam = value.getPost().getPostId().toString();
+
+        AlarmEvent event = new AlarmEvent(this, AlarmType.COMMENT, fromUser, toUser,
+                urlParam);
+        publisher.publishEvent(event);
+
+        return value;
     }
 
     @Override
     @Transactional
     public PostComment updateComment(Long commentId, String content) {
-        PostComment postComment = postCommentRepository.findById(commentId).orElseThrow(() -> new IllegalArgumentException("Invalid comment ID"));
+        PostComment postComment = postCommentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid comment ID"));
         postComment.setPostCommentContent(content);
         return postCommentRepository.save(postComment);
     }
@@ -53,6 +70,22 @@ public class PostCommentServiceImpl implements PostCommentService {
         postComment.setPost(postService.getPostById(postId));
         postComment.setUser(userService.findByUserId(userId));
         postComment.setPostCommentPreCommentId(parentCommentPreCommentId);
-        return postCommentRepository.save(postComment);
+        var value = postCommentRepository.save(postComment);
+
+        // 트랜잭션 종료 후 이벤트 발생
+        var ParentComment = postCommentRepository.findById(parentCommentPreCommentId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid comment ID"));
+        User fromUser = value.getUser();
+        User toUser = ParentComment.getUser();
+        if (fromUser.equals(toUser)) {
+            return value;
+        }
+        String urlParam = value.getPost().getPostId().toString();
+
+        AlarmEvent event = new AlarmEvent(this, AlarmType.REPLY, fromUser, toUser,
+                urlParam);
+        publisher.publishEvent(event);
+
+        return value;
     }
 }
