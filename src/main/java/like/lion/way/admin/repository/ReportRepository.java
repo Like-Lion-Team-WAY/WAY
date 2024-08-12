c4d902d608db18adc806dc72e9cfe193446efca4
hye2021 <hyebeenbyeon@gmail.com>
2024-08-12 17:08:35 +0900

    feat: implement report (#77)

---
+++ src/main/java/like/lion/way/admin/repository/ReportRepository.java
@@ -0,0 +1,10 @@
+package like.lion.way.admin.repository;
+
+import like.lion.way.admin.domain.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, Long> {
+
+}
