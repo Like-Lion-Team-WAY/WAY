57555fdf11dd1235b8d55679f95257bebb9d57dd
Firemanhyeon <ghgus990129@naver.com>
2024-08-19 11:48:42 +0900

    setting: s3 bucket Setting

---
+++ src/main/java/like/lion/way/config/S3Config.java
@@ -0,0 +1,19 @@
+package like.lion.way.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    @Bean
+    public S3Client s3Client(){
+        return S3Client.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+}
