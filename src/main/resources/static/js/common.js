a76d6fd26df0133022e8fead348b5eebe3427129
Firemanhyeon <ghgus990129@naver.com>
2024-08-27 15:03:56 +0900

    feat: main Way Click change aside tab

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -19,6 +19,7 @@ $(document).ready(function() {
         localStorage.setItem('activeTab', 'communityTab');
     });
 
+
     function getCookie(name) {
         const cookies = document.cookie.split(';');
         for (let i = 0; i < cookies.length; i++) {
