bde07159471c4c1c8c45ea7584324a2501a2a248
Firemanhyeon <ghgus990129@naver.com>
2024-09-05 15:04:23 +0900

    Update select_and_deploy.sh

--- deployment/scripts/select_and_deploy.sh
+++ deployment/scripts/select_and_deploy.sh
@@ -6,11 +6,11 @@ HOSTNAME=$(hostname)
 if [[ "$HOSTNAME" == "ip-172-31-1-210.ap-northeast-2.compute.internal" ]]; then
   # 서버1에 해당하는 JAR 파일을 배포
   echo "Deploying for Server 1"
-  cp /home/ec2-user/app/SERVER1-WAY-0.0.1-SNAPSHOT.jar /home/ec2-user/app/application.jar
+  sudo cp /home/ec2-user/app/SERVER1-WAY-0.0.1-SNAPSHOT.jar /home/ec2-user/app/application.jar
 elif [[ "$HOSTNAME" == "ip-172-31-3-37.ap-northeast-2.compute.internal" ]]; then
   # 서버2에 해당하는 JAR 파일을 배포
   echo "Deploying for Server 2"
-  cp /home/ec2-user/app/SERVER2-WAY-0.0.1-SNAPSHOT.jar /home/ec2-user/app/application.jar
+  sudo cp /home/ec2-user/app/SERVER2-WAY-0.0.1-SNAPSHOT.jar /home/ec2-user/app/application.jar
 else
   echo "Unknown server. Exiting deployment."
   exit 1
