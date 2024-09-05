b5489b30f2732702c0fd3467ae0232e4292bf5dd
Firemanhyeon <ghgus990129@naver.com>
2024-09-05 15:08:51 +0900

    Update start_server.sh

--- deployment/scripts/start_server.sh
+++ deployment/scripts/start_server.sh
@@ -5,8 +5,8 @@ echo "init" > start_server_log.txt
 # Start the new JAR file
 if [[ "$HOSTNAME" == "ip-172-31-1-210.ap-northeast-2.compute.internal" ]]; then
 serverName=myServerName kafkaGroupId=group1 nohup java -jar application.jar > applog.txt 2> errorlog.txt &
-echo "Application started." >> start_server_log.txt
+sudo echo "Application started." >> start_server_log.txt
 else
 serverName=myServerName2 kafkaGroupId=group2 nohup java -jar application.jar > applog.txt 2> errorlog.txt &
-echo "Application started." >> start_server_log.txt
+sudo echo "Application started." >> start_server_log.txt
 fi
